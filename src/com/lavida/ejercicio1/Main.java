80aaed9c5b952aef448dc684a58a4648d322d233
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2021-10-08 09:34:00 +0200

    InicioProyecto

---
+++ src/com/lavida/ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package com.lavida.ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/lavida/ejercicio2/nueva.java
@@ -0,0 +1,9 @@
+package com.lavida.ejercicio2;
+
+public class nueva {
+    public static void (String[] args){
+
+        System.out.println("Wassup");
+
+    }
+}
